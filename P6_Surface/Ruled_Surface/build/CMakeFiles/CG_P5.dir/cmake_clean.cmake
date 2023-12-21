bfae50e461ead8ebdf37ede3e937ef23f5afa7b1
BoringMan60101 <80347868+BoringMan60101@users.noreply.github.com>
2023-12-21 09:13:55 +0500

    Add lab 6

---
+++ P6_Surface/Ruled_Surface/build/CMakeFiles/CG_P5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CG_P5"
+  "CG_P5.pdb"
+  "CMakeFiles/CG_P5.dir/main.cpp.o"
+  "CMakeFiles/CG_P5.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CG_P5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
